2ae05c53985e20b7cb2025ecfeea583b0147d542
JhonatanRocha <jcristianrocha@gmail.com>
2018-10-17 08:02:59 -0300

    Validacao do Token

--- app/src/main/java/com/jhonatanrocha/whatsapp/activity/LoginActivity.java
+++ app/src/main/java/com/jhonatanrocha/whatsapp/activity/LoginActivity.java
@@ -5,6 +5,7 @@ import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
@@ -13,6 +14,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.github.rtoshiro.util.format.SimpleMaskFormatter;
 import com.github.rtoshiro.util.format.text.MaskTextWatcher;
@@ -79,9 +81,16 @@ public class LoginActivity extends Activity {
                 telefoneSemFormatacao = "5554";
                 Boolean smsEnviadoComSucesso = enviarSMS("+" + telefoneSemFormatacao, mensagemEnvio);
 
+                if(smsEnviadoComSucesso) {
+                    Intent intent = new Intent(LoginActivity.this, ValidadorActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Problema ao enviar SMS, tente novamente mais tarde!", Toast.LENGTH_LONG).show();
+                }
 
-//                HashMap<String, String> usuario = preferencias.getDadosUsuario();
-//                Log.i("Recupernando Dados:", "T: " + usuario.get("token"));
+//              HashMap<String, String> usuario = preferencias.getDadosUsuario();
+//              Log.i("Recupernando Dados:", "T: " + usuario.get("token"));
 
             }
         });
--- app/src/main/java/com/jhonatanrocha/whatsapp/activity/ValidadorActivity.java
+++ app/src/main/java/com/jhonatanrocha/whatsapp/activity/ValidadorActivity.java
@@ -2,13 +2,51 @@ package com.jhonatanrocha.whatsapp.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.github.rtoshiro.util.format.SimpleMaskFormatter;
+import com.github.rtoshiro.util.format.text.MaskTextWatcher;
 import com.jhonatanrocha.whatsapp.R;
+import com.jhonatanrocha.whatsapp.helper.Preferencias;
+
+import java.util.HashMap;
 
 public class ValidadorActivity extends Activity {
+
+    private EditText editTextValidacao;
+    private Button botaoValidar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_validador);
+
+        final SimpleMaskFormatter simpleMaskCodigoValidacao = new SimpleMaskFormatter("NNNN");
+
+        editTextValidacao = (EditText) findViewById(R.id.editTextCodigoValidacao);
+        botaoValidar = (Button) findViewById(R.id.botaoValidar);
+
+        editTextValidacao.addTextChangedListener(new MaskTextWatcher(editTextValidacao, simpleMaskCodigoValidacao));
+
+        botaoValidar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Recuperar Dados das Preferencias do Usuário
+                Preferencias preferencias = new Preferencias(ValidadorActivity.this);
+                HashMap<String, String> usuario = preferencias.getDadosUsuario();
+
+                String tokenGerado = usuario.get("token");
+                String tokenDigitado = editTextValidacao.getText().toString();
+
+                if(tokenDigitado.equals(tokenGerado)) {
+                    Toast.makeText(ValidadorActivity.this, "Token Validado com Sucesso!", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(ValidadorActivity.this, "Token Invalidado! Verifique suas mensagens SMS!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
